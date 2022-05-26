21e8274fd0a36e1b570d2bddf82de16fb1e0fa37
Jeff <regionbbs@users.noreply.github.com>
2022-05-26 09:00:36 +0800

    Remove older content and security issue items.

--- 04-sfAuction/Libraries/Common/ServiceNames.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace SFAuction.Common {
-   public static class ServiceNames {
-      public static readonly Uri AuctionServiceName = new Uri(@"fabric:/SFAuction/AuctionServiceInstance");
-   }   
-}
